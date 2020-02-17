5a3de21d9d3e579b4c4b246df73088d6e415f5d8
ivanetra <ivanekaputra24@gmail.com>
2020-02-17 20:13:33 +0700

    Add code to call variable by id

--- app/src/main/java/com/example/kalkulator/MainActivity.java
+++ app/src/main/java/com/example/kalkulator/MainActivity.java
@@ -12,10 +12,19 @@ public class MainActivity extends AppCompatActivity {
     EditText angka_pertama, angka_kedua;
     Button tambah, kurang, kali, bagi, bersihkan;
     TextView hasil;
-    
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        angka_pertama = (EditText) findViewById(R.id.angka_pertama);
+        angka_kedua = (EditText) findViewById(R.id.angka_kedua);
+        tambah = (Button) findViewById(R.id.tambah);
+        kurang = (Button) findViewById(R.id.kurang);
+        kali = (Button) findViewById(R.id.kali);
+        bagi = (Button) findViewById(R.id.bagi);
+        bersihkan = (Button) findViewById(R.id.bersihkan);
+        hasil = (TextView) findViewById(R.id.hasil);
     }
 }
