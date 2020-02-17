1acba4c02622c819db0c933d827e403a9d9c9cd7
ivanetra <ivanekaputra24@gmail.com>
2020-02-17 20:09:50 +0700

    Declare variable angka_pertama, angka_kedua, tambah, kurang, kali, bagi, bersihkan, and hasil

--- app/src/main/java/com/example/kalkulator/MainActivity.java
+++ app/src/main/java/com/example/kalkulator/MainActivity.java
@@ -3,9 +3,16 @@ package com.example.kalkulator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
+    EditText angka_pertama, angka_kedua;
+    Button tambah, kurang, kali, bagi, bersihkan;
+    TextView hasil;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
